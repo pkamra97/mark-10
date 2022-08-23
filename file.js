2ee38737fdb3cc3dffd7dbba4a83031287542c3b
pkamra97 <109149360+pkamra97@users.noreply.github.com>
2022-08-23 16:28:50 +0530

    fixed

--- file.js
+++ file.js
@@ -1,27 +1,21 @@
 const billAmount = document.querySelector("#bill-amount");
 
-// console.log(billAmount.value);
-// console.log(billAmount.value);
+console.log(billAmount.value);
+console.log(billAmount.value);
 
 const cashGiven = document.querySelector("#cash-given")
 const checkButton = document.querySelector("#check-button");
 const message=document.querySelector("#error-message");
 const noOfnotes=document.querySelectorAll(".no_of_notes");
-
+const currency=[2000,500,100,20,10,5,1];
 // checkButton.addEventListener("click",()=> console.log("clicked "))
 
 checkButton.addEventListener("click",function validateBillAndCash(){
-   
-    // console.log(billAmount.value);
-    // console.log(cashGiven.value);
-
-    //array of currency's
-    const currency=[2000,500,100,20,10,5,1];
     hide_msg();
     if(billAmount.value>0){
         console.log(billAmount.value);
         console.log(cashGiven.value);
-        if(cashGiven.value >= billAmount.value)
+        if(Number(cashGiven.value) >= Number(billAmount.value))
         {
             const amountToBeReturned=cashGiven.value-billAmount.value;
             calculateChange(amountToBeReturned);
